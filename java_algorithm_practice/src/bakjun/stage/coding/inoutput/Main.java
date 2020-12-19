9bc9abb7de02c95beb66e1f878f2533ec6e30c06
kimjuho <kim1jo2ho3@gmail.com>
2020-12-19 22:49:04 +0900

    commit

---
+++ java_algorithm_practice/src/bakjun/stage/coding/inoutput/Main.java
@@ -0,0 +1,12 @@
+package bakjun.stage.coding.inoutput;
+
+public class Main {
+	public static void main(String[] args) {
+		Input inp = new Input();
+		
+		inp.first();
+		inp.second();
+		inp.third();
+		
+	}
+}
