c3be04d26b8d194515467d8f52582d74a76dba55
kim1jo2ho3 <kim1jo2ho3@naver.com>
2020-12-22 10:42:08 +0900

    commit

---
+++ java_algorithm_practice/src/chap01/SumForPos.java
@@ -0,0 +1,14 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class SumForPos {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		int n ;
+		
+		System.out.println("1부터 n까지의 합을 구합니다");
+		
+	}
+}
