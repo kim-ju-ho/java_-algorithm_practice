6ee22e95414a99b0c7d4a0cf3bc4711f12298596
kim1jo2ho3 <kim1jo2ho3@naver.com>
2020-12-23 18:01:26 +0900

    commit

--- java_algorithm_practice/src/chap01_EX/EX_12.java
+++ java_algorithm_practice/src/chap01_EX/EX_12.java
@@ -7,10 +7,12 @@ public class EX_12 {
 			System.out.printf("%3d",i);
 		}
 		System.out.println();
-		System.out.printf("--+------------------");
+		System.out.printf("--+------------------------");
 		for(int i =1;i<=9;i++) {
 			System.out.println();
+			System.out.print(i+" |");
 			for(int j =1;j<=9;j++) {
+				
 				System.out.printf("%3d",i * j);
 			}
 			System.out.println();
