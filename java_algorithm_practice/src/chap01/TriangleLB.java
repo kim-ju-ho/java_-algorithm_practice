82338c4a9f8cb8f0b96feb1f9a3fb250bda4b426
kim1jo2ho3 <kim1jo2ho3@naver.com>
2020-12-23 18:23:05 +0900

    commit

---
+++ java_algorithm_practice/src/chap01/TriangleLB.java
@@ -0,0 +1,21 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class TriangleLB {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		int n;
+		
+		System.out.println("왼쪽 아래가 직각인 이등변 삼각형을 출력합니다.");
+		
+		do {
+			System.out.print("몇단 삼각형 입니까?");
+			n = stdIn.nextInt();
+			
+		}while(n <=0);
+		
+		for(int)
+	}
+}
---
+++ java_algorithm_practice/src/chap01_EX/EX_14.java
@@ -0,0 +1,18 @@
+package chap01_EX;
+
+import java.util.Scanner;
+
+public class EX_14 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("사각형을 출력합니다");
+		System.out.print("단수 : ");
+		int a = sc.nextInt();
+		for(int i =0;i<a;i++) {
+			for(int j=0;j<a;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
