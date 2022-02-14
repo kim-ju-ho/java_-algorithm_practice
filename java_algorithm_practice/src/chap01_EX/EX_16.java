1576cb2edc2ab8c7afddd4f6d883ffed488310ec
kim1jo2ho3 <kim1jo2ho3@naver.com>
2022-02-14 14:57:32 +0900

    ex-16

--- java_algorithm_practice/src/chap01_EX/EX_16.java
+++ java_algorithm_practice/src/chap01_EX/EX_16.java
@@ -3,6 +3,19 @@ package chap01_EX;
 public class EX_16 {
 	
 	static void spira(int n) {
-		
+		for(int i=0;i<4;i++){
+			for(int j=0;j<3-i;j++){
+				System.out.print(" "); 
+			}
+			for(int j=0;j<2*i+1;j++){
+				System.out.print("*"); 
+			}
+			System.out.println("");
+		} 
+	    
+	}
+	
+	public static void main(String[] args) {
+		spira(5);
 	}
 }
