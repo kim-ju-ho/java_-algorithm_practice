1c5bcf6f7cc76fe9aafe4af84f7ae8cb26fde83f
kim-ju-ho <kim1jo2ho3@gmail.com>
2022-02-14 16:30:57 +0900

    ex-17

--- java_algorithm_practice/src/chap01_EX/EX_17.java
+++ java_algorithm_practice/src/chap01_EX/EX_17.java
@@ -3,6 +3,17 @@ package chap01_EX;
 public class EX_17 {
 
 	static void npira(int n) {
-		
+		for(int i=0;i<n;i++){
+			for(int j=0;j<3-i;j++){
+				System.out.print(" "); 
+			}
+			for(int j=0;j<2*i+1;j++){
+				System.out.print(i+1); 
+			}
+			System.out.println("");
+		} 
+	}
+	public static void main(String[] args) {
+		npira(4);
 	}
 }
